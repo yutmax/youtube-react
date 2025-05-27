016c7c9af1f392eef243cf84cc83bb98c021a8ee
yutmax <dealplug72@gmail.com>
2025-05-27 15:38:39 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <main class="page"></main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
